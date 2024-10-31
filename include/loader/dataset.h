603e86810f1caa6fe03816d6b7182b2fedb3ecd4
long89kev <tamlongnguyen1234@gmail.com>
2024-10-31 23:42:35 +0700

    Implement data and label initialization in TensorDataset and add shape retrieval methods

--- include/loader/dataset.h
+++ include/loader/dataset.h
@@ -74,6 +74,10 @@ public:
     TensorDataset(xt::xarray<DType> data, xt::xarray<LType> label){
         /* TODO: your code is here for the initialization
          */
+        this->data = data;
+        this->label = label;
+        if(data.dimension() == 0) data_shape = {}; else data_shape.assign(data.shape().begin(), data.shape().end());
+        if(label.dimension() == 0) label_shape = {}; else label_shape.assign(label.shape().begin(), label.shape().end());
     }
     /* len():
      *  return the size of dimension 0
@@ -81,7 +85,7 @@ public:
     int len(){
         /* TODO: your code is here to return the dataset's length
          */
-        return 0; //remove it when complete
+        return data_shape[0];
     }
     
     /* getitem:
@@ -90,15 +94,25 @@ public:
     DataLabel<DType, LType> getitem(int index){
         /* TODO: your code is here
          */
+        if(index >= data_shape[0]) throw std::runtime_error("Index out of range");
+        if(label_shape.size() == 0) {
+            if(label.size() == 0) return DataLabel<DType, LType>(xt::view(data, index), {});
+            else return DataLabel<DType, LType>(xt::view(data, index), label);
+        }
+        return DataLabel<DType, LType>(xt::view(data, index), xt::view(label, index));
     }
     
     xt::svector<unsigned long> get_data_shape(){
         /* TODO: your code is here to return data_shape
          */
+        return data_shape;
     }
     xt::svector<unsigned long> get_label_shape(){
         /* TODO: your code is here to return label_shape
          */
+        bool is_label = label_shape.size() > 0;
+        if(is_label) return label_shape;
+        return {};
     }
 };
 
