195af9e992dc2dad45af15eeee8a11d852573512
long89kev <tamlongnguyen1234@gmail.com>
2024-11-01 10:49:17 +0700

    Implement dataset loading and normalization in DSFactory for training, validation, and test sets

--- src/ann/dataset/DSFactory.cpp
+++ src/ann/dataset/DSFactory.cpp
@@ -32,6 +32,43 @@ xmap<string, TensorDataset<double, double>*>* DSFactory::get_datasets_3cc(){
     string train_file = (dataset_path / fs::path("3c_train.npy")).string();
     string valid_file = (dataset_path / fs::path("3c_valid.npy")).string();
     string test_file = (dataset_path / fs::path("3c_test.npy")).string();
+
+    //load data from files
+    // tables: the first two columns are data (i.e., x and y) and the last is target
+    xt::xarray<double> train_table = xt::load_npy<double>(train_file);
+    xt::xarray<double> valid_table = xt::load_npy<double>(valid_file);
+    xt::xarray<double> test_table = xt::load_npy<double>(test_file);
+
+    xt::xarray<double> mu, sigma;
+    estimate_params(xt::view(train_table, xt::all(), xt::range(0,2)), mu, sigma);
+
+    cout << shape2str(train_table.shape()) << endl;
+    xt::xarray<double> X_train = normalize(xt::view(train_table, xt::all(), xt::range(0,2)), mu, sigma );
+    xt::xarray<double> t_train = xt::view(train_table, xt::all(), -1);
+    xt::xarray<double> T_train = onehot_enc(xt::cast<unsigned long>(t_train), 3);
+
+    xt::xarray<double> X_valid = normalize(xt::view(valid_table, xt::all(), xt::range(0,2)), mu, sigma  );
+    xt::xarray<double> t_valid = xt::view(valid_table, xt::all(), -1);
+    xt::xarray<double> T_valid = onehot_enc(xt::cast<unsigned long>(t_valid), 3);
+
+    xt::xarray<double> X_test = normalize(xt::view(test_table, xt::all(), xt::range(0,2)), mu, sigma  );
+    xt::xarray<double> t_test = xt::view(test_table, xt::all(), -1);
+    xt::xarray<double> T_test = onehot_enc(xt::cast<unsigned long>(t_test), 3);
+
+    TensorDataset<double, double>* train_ds = new TensorDataset<double, double>(X_train, T_train);
+    TensorDataset<double, double>* valid_ds = new TensorDataset<double, double>(X_valid, T_valid);
+    TensorDataset<double, double>* test_ds = new TensorDataset<double, double>(X_test, T_test);
+
+    xmap<string, TensorDataset<double, double>*>* pMap =
+        new xmap<string, TensorDataset<double, double>*>(
+            &stringHash,
+            0.75, //load-factor
+            0, //value-comparator: use ==
+            xmap<string, TensorDataset<double, double>*>::freeValue);
+    pMap->put("train_ds", train_ds);
+    pMap->put("valid_ds", valid_ds);
+    pMap->put("test_ds", test_ds);
+    return pMap;
 }
 
 
